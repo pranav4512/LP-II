b1be1e9ee724fd2a9664d35a8b7c36772e08c8f1
pranav4512 <borsepranav123@gmail.com>
2025-05-06 09:20:34 +0530

    Create Input.py

---
+++ Input.py
@@ -0,0 +1,13 @@
+# Taking multiple inputs from the user
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+email = input("Enter your email: ")
+city = input("Enter your city: ")
+
+# Displaying the collected information
+print("\n--- User Information ---")
+print("Name: ", name)
+print("Age: ", age)
+print("Email: ", email)
+print("City: ", city)
